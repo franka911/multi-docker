a7198d0311bf0cf811553be13f23f82f9f7af866
franka911 <franczeskaum@interia.pl>
2021-01-04 17:06:49 +0100

    updated client

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default () => {
+
+    return (
+        <div>
+            Im some other page!
+            <link to ="/">Go back home </link> 
+        </div>
+    );
+};
